2c518c29f43139dd86f2bb81c18fd1fc169b4a62
surya <surya.snath08@gmail.com>
2016-08-14 15:08:56 +1200

    Add sass instead css

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
+import Heading from './heading.jsx'
 
 class App extends Component {
 
 	render() {
 		return (
 			<div>
-			<h1>This is my project</h1>
+			<Heading />
+			<p>This is my project</p>
 			</div>
 			)
 	}
---
+++ client/components/heading.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Heading extends Component {
+
+	render() {
+		return (
+			<div>
+			<h1>Gluten Free Products </h1>
+			</div>
+			)
+	}
+}
+
+export default Heading

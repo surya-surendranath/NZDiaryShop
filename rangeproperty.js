46939a89adf118c8726ff9e4c22c96288595443e
surya-surendranath <surya.snath08@gmail.com>
2016-09-27 11:09:22 +1300

    animation

--- rangeproperty.js
+++ rangeproperty.js
@@ -11,4 +11,18 @@ function myFunction() {
     }
     document.getElementById("demo").innerHTML = txt;
 }
+function myMove() {
+    var elem = document.getElementById("animate");
+    var pos = 0;
+    var id = setInterval(frame, 5);
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+        }
+    }
+}
 </script>
